32523929416dfce8ba5d86942b278d2533c58e7b
novo <cjq19890707@163.com>
2016-04-17 23:16:22 +0800

    modified:   async/cmock_func_mocker_test.cc

--- async/cmock_func_mocker_test.cc
+++ async/cmock_func_mocker_test.cc
@@ -24,12 +24,13 @@ TEST(FunctionMockersTest, TestCb)
 	{
 		FooFunctionMock mock;
 		callback cb = cb_stub;
-		EXPECT_FUNCTION_CALL(mock, (_)).WillOnce(DoAll(InvokeArgument<0>(), Return(0)));
+		EXPECT_FUNCTION_CALL(mock, (_)).WillOnce(DoAll(InvokeArgument<0>(), Return(-1)));
+		ASSERT_EQ(-1, foo(cb_real));
+		EXPECT_FUNCTION_CALL(mock, (_)).WillOnce(Invoke(foo_stub));
 		ASSERT_EQ(0, foo(cb_real));
+		EXPECT_FUNCTION_CALL(mock, (_)).WillOnce(SetArgPointee<0>(cb_stub));
+		ASSERT_EQ(1, foo(cb_real));
 	}
 
 	ASSERT_EQ(1, foo(cb_real));
 }
-
-
-//example
